9bac5ce23f5242f9cc7923969e8575083e6cfa8c
UstehKenny <carosiim.sdco@gmail.com>
2019-02-11 11:38:20 -0600

    Vista Prueba
    Se agregan los escudos de las constancias, así como los títulos de las constancias

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,7 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Base de Datos</title>
 </head>
+<style>
+    div.container {
+        text-align:center;
+    }
+    body {
+  font-family: Arial, Helvetica, Sans-serif;
+  
+  background-color: #b3cccc;;
+  
+}
+</style>
 <body>
+    <!--body style="background-image:url(http://www.ingenieria.unam.mx/nuestra_facultad/images/institucionales/escudo_fi_color.png);background-attachment: fixed; background-size: cover; background-repeat: no-repeat;background-position: center;" -->
+    <div class="container">
+        <div>
+            <div><img src="http://www.ingenieria.unam.mx/nuestra_facultad/images/institucionales/escudo_fi_color.png" width="150" height="184" align="left"><img src="https://congreso.seguridad.unam.mx/2016/sites/default/themes/theme2016/images/unam_negro.png" width="160" height="170" align="right"></div>
+            
+        </div>
+            <h2>Universidad Nacional Autónoma de México</h2>
+            <h3>Facultad de Ingeniería</h3>
+            <h3>Secretaría de Apoyo a la Docencia</h3>
+            <h2>Centro de Docencia</h2>
+
+            <a href="{{ url('/base') }}" class="btn btn-default">Descargar PDF</a>
+    </div>
+<!--
     <h1>Lista de nombres</h1>
     <table border="1">
 		<caption>Lista</caption>
@@ -16,16 +41,16 @@
             <th>Email</th>
 		</tr>
         <tbody>
-        <!-- //foreach -->
+        //foreach 
         
 		<tr>
 			
 		</tr>
         
-        <!-- //endforeach -->
+         //endforeach 
         </tbody>
 	</table>
-        <p></p>
-    <a href="{{ url('/base') }}" class="btn btn-default">Descargar PDF</a>
+        <p></p> -->
+    
 </body>
 </html>
