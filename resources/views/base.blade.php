17e73a4941560a0597a328b37202f0d6a123dcda
UstehKenny <carosiim.sdco@gmail.com>
2019-02-11 12:09:20 -0600

    Vista
    Agrego background de la constancia.

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,47 +10,31 @@
     div.container {
         text-align:center;
     }
-    body {
+body {
   font-family: Arial, Helvetica, Sans-serif;
   
-  background-color: #b3cccc;;
+  background-image: url(http://www.centrodedocencia.unam.mx/images/logos/logo_cdd_2.png);background-attachment: fixed;background-size: 200px 200px; background-repeat: no-repeat;background-position: center;;
   
+}
+h4 {
+    margin-left:10; 
+    margin-right:10;
+    font-style:italic;
 }
 </style>
 <body>
     <!--body style="background-image:url(http://www.ingenieria.unam.mx/nuestra_facultad/images/institucionales/escudo_fi_color.png);background-attachment: fixed; background-size: cover; background-repeat: no-repeat;background-position: center;" -->
     <div class="container">
         <div>
-            <div><img src="http://www.ingenieria.unam.mx/nuestra_facultad/images/institucionales/escudo_fi_color.png" width="150" height="184" align="left"><img src="https://congreso.seguridad.unam.mx/2016/sites/default/themes/theme2016/images/unam_negro.png" width="160" height="170" align="right"></div>
+            <div><img src="http://www.ingenieria.unam.mx/nuestra_facultad/images/institucionales/escudo_fi_color.png" width="150" height="184" align="right"><img src="https://congreso.seguridad.unam.mx/2016/sites/default/themes/theme2016/images/unam_negro.png" width="160" height="170" align="left"></div>
             
         </div>
             <h2>Universidad Nacional Autónoma de México</h2>
             <h3>Facultad de Ingeniería</h3>
             <h3>Secretaría de Apoyo a la Docencia</h3>
             <h2>Centro de Docencia</h2>
-
+            <h4>"Ing. Gilberto Borja Navarrete"</h4>
             <a href="{{ url('/base') }}" class="btn btn-default">Descargar PDF</a>
     </div>
-<!--
-    <h1>Lista de nombres</h1>
-    <table border="1">
-		<caption>Lista</caption>
-		<tr>
-			<th>ID</th>
-			<th>Nombre</th>
-            <th>Email</th>
-		</tr>
-        <tbody>
-        //foreach 
-        
-		<tr>
-			
-		</tr>
-        
-         //endforeach 
-        </tbody>
-	</table>
-        <p></p> -->
-    
 </body>
 </html>
