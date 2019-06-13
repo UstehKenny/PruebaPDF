87908d80e91302dc0c0418623a0bf92a69aabef4
UstehKenny <carosiim.sdco@gmail.com>
2019-06-13 12:12:43 -0500

    1
    2

--- resources/views/concentrado.blade.php
+++ resources/views/concentrado.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -15,37 +16,16 @@ body {
   font-family: Arial, Helvetica, Sans-serif;
   align-items: center;
   font-size: 18px;
-  
 }
-hr{
-    border: 1px solid #000;
-  	border-spacing: 0;
-  }
-#cuerpo{
-  background-image: url("img/m.png");
-  background-repeat: no-repeat;
-  background-position: absolute;
-}
-#mayusculas{
-	text-transform: uppercase;
-}
-#contenido{
-    padding:15px 60px 15px;
- 
-   
-}
-#renglonDoble, #mayusculas{
-	 border: 1px solid white;
-}
-#normal{
-	
-   border-spacing: 0;
+.contenido{
+	font-size: 12px; 
 }
 .encabezado{
 	font-size: 20px;
 }
 
 </style>
+
 <body>
 <div style="height: 90%">
 	<div height="10%">
@@ -53,25 +33,45 @@ hr{
 			UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO
 		</div>
 		
-		<table style="width: 100%" align="center" id="encabezado" height="5%">
-			<tr id="normal">
-				<td  width="10%" id="normal">
-					<img src="http://www.ingenieria.unam.mx/nuestra_facultad/images/institucionales/escudo_fi_color.png" alt="" align="right" height="120">
-				</td>
-				<td  align="center" class="encabezado">
-			        FACULTAD DE INGENIERÍA<br/>
-			        Secretaría de Apoyo a la Docencia<br/>
-			        CENTRO DE DOCENCIA <br/>
-			        "Ing. Gilberto Borja Navarrete"<br/>
-				</td>
-				<td align="center" width="15%" id="normal">
-					<img src="http://www.centrodedocencia.unam.mx/images/logos/logo_cdd_2.png" alt="" align="right" height="120">
-				</td> 
-			</tr>
-		</table>
+			<table style="width: 100%" align="center" id="encabezado" height="5%">
+				<tr id="normal">
+					<td  width="10%" id="normal">
+						<img src="http://www.ingenieria.unam.mx/nuestra_facultad/images/institucionales/escudo_fi_color.png" alt="" align="right" height="120">
+					</td>
+					<td  align="center" class="encabezado">
+						FACULTAD DE INGENIERÍA<br/>
+						Secretaría de Apoyo a la Docencia<br/>
+						CENTRO DE DOCENCIA <br/>
+						"Ing. Gilberto Borja Navarrete"<br>
+					</td>
+					<td align="center" width="15%" id="normal">
+						<img src="http://www.centrodedocencia.unam.mx/images/logos/logo_cdd_2.png" alt="" align="right" height="120">
+					</td> 
+				</tr>
+			</table>
         <br>
-		<hr/>
-
+			<table>
+				<div align="center">
+					<div style="float: left; width: 100%; font-size: 22px;">{Semestre}</div>
+					<div style="float: right; width: 5%;height: 10%; font-size: 12px;">Máx Min</div>
+					<div style="float: right; width: 45%;">h</div>
+					<br>
+					<hr>
+				</div>
+			</table>
 	</div>
+
+	<div class="contenido">
+		<table style="width: 100%">
+			<td WIDTH="60">{MENSAJE}</td>
+			<td WIDTH="70">{Nombre de los cursos a impartir}</td>
+			<td WIDTH="80">{Nombre de los instructores}</td>
+			<td WIDTH="53">{Fecha Inicio/Fin}</td>
+			<td WIDTH="30">{Horarios}</td>
+			<td WIDTH="45">{Lugar donde se impartirán los cursos}</td>
+			<td WIDTH="5">{Cupo}</td>
+		</table>
+	</div>
+</div>
 </body>
 </html>
