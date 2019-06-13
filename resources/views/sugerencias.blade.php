de80a2f8d65a376dc732afa12b43079da32c4518
UstehKenny <carosiim.sdco@gmail.com>
2019-06-13 12:51:52 -0500

    Update sugerencias.blade.php

---
+++ resources/views/sugerencias.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sugerencias</title>
+</head>
+<style>
+div.container {
+        text-align:center;
+    }
+html{
+	width:100%;
+}
+body {
+  font-family: Arial, Helvetica, Sans-serif;
+  align-items: center;
+  font-size: 18px;
+}
+.titulo{
+    font-size:20px;
+}
+.contenido{
+	font-size: 15px; 
+}
+.encabezado{
+	font-size: 20px;
+}
+
+</style>
+
+<body>
+<div style="height: 90%">
+	<div height="10%">
+		<div align="center" class="encabezado">
+			UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO
+		</div>
+		
+			<table style="width: 100%" align="center" id="encabezado" height="5%">
+				<tr id="normal">
+					<td  width="10%" id="normal">
+						<img src="http://www.ingenieria.unam.mx/nuestra_facultad/images/institucionales/escudo_fi_color.png" alt="" align="right" height="120">
+					</td>
+					<td  align="center" class="encabezado">
+						FACULTAD DE INGENIERÍA<br/>
+						Secretaría de Apoyo a la Docencia<br/>
+						CENTRO DE DOCENCIA <br/>
+						"Ing. Gilberto Borja Navarrete"<br>
+                        Sugerencias o comentarios <br>
+					</td>
+					<td align="center" width="15%" id="normal">
+						<img src="http://www.centrodedocencia.unam.mx/images/logos/logo_cdd_2.png" alt="" align="right" height="120">
+					</td> 
+				</tr>
+			</table>
+       
+			<table>
+                <div align="center">
+                <br>
+					<div style="float: left; width: 90%; font-size: 22px;">{Semestre}</div><br><br>
+                    <div style="float: right; width: 15%;font-size: 12px;">Proceso/Acuerdo o acción</div>
+                    <div style="float: right; width: 80%; font-size: 13px;">Sugerencia o recomendación</div>
+					<br>
+					<hr>
+				</div>
+			</table>
+	</div>
+
+	<div>
+		<table style="width: 100%">
+            <tr>
+                <th class="titulo">{Curso}</th>
+                <br><br>
+            </tr>
+			<tr>
+                <td class="contenido">{Nombre del instructor}</td>
+                <td class="contenido">{Sugerencia}</td>
+                <td class="contenido" style="align=center">{P/A}</td>
+            </tr>
+            
+		</table>
+	</div>
+</div>
+</body>
+</html>
